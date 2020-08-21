98f0cc196094c81526b3a6d09a4a811e7724735d
slightlyharp <122560282@qq.com>
2020-08-21 17:33:30 +0800

    add max loan amount

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -3,12 +3,16 @@ public class Loan {
     private int amount;
     private boolean isPaid;
     private double interestRate;
+    private static int maxLoanAmount = 0;
 
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
         this.interestRate = interestRate;
         this.isPaid = false;
+        if (amount > maxLoanAmount){
+            maxLoanAmount = amount;
+        }
     }
 
     public void setAsPaid() {
@@ -29,5 +33,9 @@ public class Loan {
 
     public double getProfit(){
         return amount*interestRate;
+    }
+
+    public static int getMaxLoanAmount(){
+        return maxLoanAmount;
     }
 }
